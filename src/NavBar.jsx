844bf2f94af9765ac006d00f506b5ae43026257b
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-29 17:05:36 +0100

    try to fix bugs

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 
 const Navbar = () => {
   return (
-    
+    <>
     <nav>
       <ul className="nav-links">
         <li className="nav-item"><a href="#"><img src="./img/iconmonstr-home-1.svg" alt="home icon" /></a></li>
@@ -12,6 +12,9 @@ const Navbar = () => {
         <li className="nav-item"><a href="#"><img src="./img/iconmonstr-user-6.svg" alt="user profile icon" /></a></li>
       </ul>
     </nav>
+    
+    </>
+    
   );
 };
 
