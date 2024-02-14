b07d8783e3bb1a2a463a87b43bc20f71175d3853
Lucas Beauloi <l.beauloi@gmail.com>
2024-02-14 11:37:17 +0100

    discover page working

--- src/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SearchBar = function () {
-    return(
-        <input type="text" placeholder="        Search here..." /> //pour mettre la loupe search comme image, il faut la mettre en background via le scss
-    );
-    
-};
-export default SearchBar
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,16 +1,36 @@
-import SearchBar from "../SearchBar";
-import FetchingTrending from "../FetchingTrending";
+import React, { useState, useEffect } from 'react';
 
-const Discover = () => {
+function Discover() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('run');
 
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}`, {
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNWM5MjYzNmMwOTliZDA2OGE4OGY3OGM0YmNjZjQ2YyIsInN1YiI6IjY1NjYwMmU1MTU2Y2M3MDE0ZTY1ZmZhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0j7NvX1aIpAbGQfwYX1hafyWR7o7kQcYKmkQj1y0UdA'
+      }
+    })
+      .then(response => response.json())
+      .then(data => setData(data.results));
+  }, [searchTerm]);
+
+  const handleSearchChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
-    <>
-      <SearchBar />
-      <FetchingTrending/>  
-    </>
+    <div>
+      <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Search for a movie..." />
+      {data.map((movie) => (
+        <div className='containerDiscover'>
+          <h2>{movie.title}</h2>
+          <img className="imageDiscover" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          <p>{movie.overview}</p>
+        </div>
+      ))}
+    </div>
   );
-};
+}
 
 export default Discover;
 
